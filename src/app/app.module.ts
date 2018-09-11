ac7dc20036539dff8e152b8b8bf9c0991ebd5a7d
Dominic Martinez <martindo@b18827.local>
2018-09-11 14:18:46 -0600

    add file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/login/login.component';
+import { ApprenticeDashComponent } from './apprentice-dash/apprentice-dash.component';
+import { SupervisorDashComponent } from './supervisor-dash/supervisor-dash.component';
+import { ReportComponent } from './report/report.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ApprenticeDashComponent,
+    SupervisorDashComponent,
+    ReportComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
